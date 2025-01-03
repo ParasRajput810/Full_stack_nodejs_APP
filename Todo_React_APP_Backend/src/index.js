2930b7cfe1cf0140cbf9317ed86df6ae32b82e39
ParasRajput810 <paraswork810@gmail.com>
2025-01-03 15:25:14 +0530

    bugfixes

--- Todo_React_APP_Backend/src/index.js
+++ Todo_React_APP_Backend/src/index.js
@@ -3,18 +3,13 @@ const{PORT} = require("./config/serverconfig");
 const body_parser = require("body-parser");
 const apiroutes = require("./routers/index");
 const cors = require("cors");
-const corsOptions = {
-    origin: 'http://todoApp-frontend:3000', // Replace with the frontend's exact origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-    credentials: true, // If you are using cookies or authentication
-};
+
 const settingup = async ()=>{
     const app = express();
 
     app.use(body_parser.json());
     app.use(body_parser.urlencoded({extended:true}));
-    app.use(cors(corsOptions));
+    app.use(cors());
     app.use("/api", apiroutes);
 
     
